9e55a4bbb154a12a3e4d7d622c895e2b43b867aa
Dididoo-mk <146987007+Dididoo-mk@users.noreply.github.com>
2025-02-27 13:10:07 -0500

    okay

--- global.js
+++ global.js
@@ -1,5 +1,16 @@
+
 console.log("IT’S ALIVE!");
 
-function $$ (selector, context = document) {
-	return Array.from(context.querySelectorAll(selector));
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
 }
+
+
+let navLinks = $$("nav a");
+
+
+let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname);
+
+
+currentLink?.classList.add("current");
