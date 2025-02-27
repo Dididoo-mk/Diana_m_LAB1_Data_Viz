d6b29990e9874cb9662210b0957ee8118f4211b1
Dididoo-mk <146987007+Dididoo-mk@users.noreply.github.com>
2025-02-27 13:43:28 -0500

    okay

--- global.js
+++ global.js
@@ -12,8 +12,8 @@ document.body.prepend(nav);
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
-  if (!ARE_WE_HOME && !url.startsWith("http") && url !== "") {
-    url = "../" + url;
+  if (!ARE_WE_HOME && !url.startsWith("http")) {
+    url = (url === "" ? "../index.html" : "../" + url);
   }
   let a = document.createElement("a");
   a.href = url;
