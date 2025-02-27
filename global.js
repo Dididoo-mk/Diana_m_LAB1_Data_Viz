202d7c492ef25aaed643f88cd40f4f5f7c6d8974
Dididoo-mk <146987007+Dididoo-mk@users.noreply.github.com>
2025-02-27 13:26:03 -0500

    okay

--- global.js
+++ global.js
@@ -1,16 +1,28 @@
-
-console.log("IT’S ALIVE!");
-
-
-function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));
+console.log("Step 3: Automatic Navigation Menu");
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "resume/", title: "Resume" },
+  { url: "https://github.com/Dididoo-mk", title: "GitHub" }
+];
+const ARE_WE_HOME = document.documentElement.classList.contains("home");
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  if (!ARE_WE_HOME && !url.startsWith("http") && url !== "") {
+    url = "../" + url;
+  }
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add("current");
+  }
+  if (a.host !== location.host && url.startsWith("http")) {
+    a.target = "_blank";
+  }
+  nav.append(a);
 }
-
-
-let navLinks = $$("nav a");
-
-
-let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname);
-
-
-currentLink?.classList.add("current");
